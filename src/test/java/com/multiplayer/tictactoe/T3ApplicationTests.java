716137d2cd26e252f5f28fb2f360f83e6ece9fd5
frhn9 <just_fadhyl@hotmail.co.id>
2025-09-30 10:26:40 +0700

    init project

---
+++ src/test/java/com/multiplayer/tictactoe/T3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplayer.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
